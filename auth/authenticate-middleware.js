74ae21bb4d4163718e94ef2de2c14a91e5d937cb
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-10-18 13:19:56 -0400

    Added middleware to users rotuer

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -7,7 +7,7 @@
 const bcrypt = require('bcryptjs');
 const Users = require('../users/user-model');
 
-module.exports = function restricted(req, res, next) {
+module.exports = function authenticate(req, res, next) {
   const { username, password } = req.headers;
 
   if( username && password ) {
--- users/users-router.js
+++ users/users-router.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 //Bring in the functionality
 const Users = require('./user-model');
 
+// Bring in middleware 
+const authenticate = require('../auth/authenticate-middleware');
 //Users route to display all users
 
-router.get('/users', (req, res)=> {
+router.get('/users', authenticate, (req, res)=> {
 
     //Checks to display the user logged in viewing users
     console.log('username', req.session.username);
