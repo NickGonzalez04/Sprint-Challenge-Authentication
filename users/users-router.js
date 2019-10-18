3a399a08a7ccb899ea4b400978808882d98d6b64
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-10-18 12:47:24 -0400

    Created a user router to see that users are being added to database

--- users/users-router.js
+++ users/users-router.js
@@ -2,3 +2,21 @@ const router = require('express').Router();
 
 
 //Bring in the functionality
+const Users = require('./user-model');
+
+//Users route to display all users
+
+router.get('/users', (req, res)=> {
+
+    //Checks to display the user logged in viewing users
+    console.log('username', req.session.username);
+
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+});
+
+
+module.exports = router;
