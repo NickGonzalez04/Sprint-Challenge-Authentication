3222ad43f17b1ab112d9aba888d283b09230ee44
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-10-18 12:51:13 -0400

    Created Session configuration

--- api/server.js
+++ api/server.js
@@ -9,12 +9,31 @@ const jokesRouter = require('../jokes/jokes-router.js');
 
 const server = express();
 
+
+const sessionConfiguration = {
+    name: 'thisthatSprintSession!',
+    secret: 'yeathisthatSprintSessionsecret',
+        cookie: {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60,
+            secure: false,
+        },
+        resave: false,
+        saveUninitialized: true,
+}
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(sessions(sessionConfiguration));
 
 server.use('/api/auth', authRouter);
 server.use('/api/loggedin',userRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+// Test Router
+server.get('/tests', (req,res) => {
+    res.status(200).json({ message: 'In there like swim wear' });
+})
 
 module.exports = server;
