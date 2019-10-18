5d968c6d54e93574efbe004410440441fbcbe1da
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-10-18 15:49:03 -0400

    Fixed the testing as it was wrong

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,42 +1,45 @@
-const request = require('supertest');
-
 const server = require('./server');
 
-
-describe('POST /api/auth/register', () => {
-    it('should return 200 http status code', () => {
-        request(server)
-        .post('/register')
-        .then(response => {
-            expect(response.status).toBe(200);
-            
-        });
-    });
-    expect(response.body).toBe({ api: 'up'});
-});
-
-describe('POST /api/auth/login', () => {
-    it('should return 200 http status code', () => {
-        request(server)
-        .post('/login')
-        .auth('username', 'password')
-        .then(response => {
-            expect(response.status).toBe(200);
-            
-        });
-    });
-});
+const request = require('supertest');
 
 
-describe('GET /api/loggedin/users', () => {
-    it('should return 200 http status code', () => {
-        request(server)
-        .get('/users')
-        .auth('username', 'password')
-        .then(response => {
-            expect(response.status).toBe(200);
-            
-        });
-    });
+
+describe('/login', () => {
     
-});
+    it('should return a JSON object', async () => {
+        const response = await request(server).post('/api/auth/login');
+
+        expect(response.type).toEqual('application/json');
+    })
+
+    it('should return status 200', async () => {
+        const expectedStatusCode = 200;
+        const response = await request(server).post('/api/auth/login')
+        .send({
+            "username": "Franky",
+            "password": "pass"
+        })
+        expect(response.status).toEqual(expectedStatusCode);
+    })
+})
+
+
+
+describe('/register', () => {
+    
+    it('should return a JSON object', async () => {
+        const response = await request(server).post('/api/auth/login');
+
+        expect(response.type).toEqual('application/json');
+    })
+
+    it('should return status 200', async () => {
+        const expectedStatusCode = 200;
+        const response = await request(server).post('/api/auth/login')
+        .send({
+            "username": "Franky",
+            "password": "pass"
+        })
+        expect(response.status).toEqual(expectedStatusCode);
+    })
+})
