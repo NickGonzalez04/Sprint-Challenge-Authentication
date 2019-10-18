2f306e977b4b2f5eb3e5c0103341e191a6f0304c
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-10-18 13:15:54 -0400

    Wrote out middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,27 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+const bcrypt = require('bcryptjs');
+const Users = require('../users/user-model');
+
+module.exports = function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if( username && password ) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+            next();
+      } else {
+       res.status(401).json({ you: 'shall not pass!' });
+         }
+      })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+    }    else {
+    res.status(400).json({ message: 'Please provide valid Credentials '})
+    }
 };
